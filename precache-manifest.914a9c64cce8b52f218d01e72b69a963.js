bbaf71e20d8223583923f84f889568d20cc0189f
Marnus Weststrate <marnusw@gmail.com>
2019-01-01 19:37:47 +0000

    Switch from gulp build to create-react-app

---
+++ precache-manifest.914a9c64cce8b52f218d01e72b69a963.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcf691a1433b935ad718",
+    "url": "/react-css-transition-replace/static/js/runtime~main.dcf691a1.js"
+  },
+  {
+    "revision": "985c8e940711aba90fde",
+    "url": "/react-css-transition-replace/static/js/main.985c8e94.chunk.js"
+  },
+  {
+    "revision": "95ee6e4dfee888cfe2ea",
+    "url": "/react-css-transition-replace/static/js/1.95ee6e4d.chunk.js"
+  },
+  {
+    "revision": "985c8e940711aba90fde",
+    "url": "/react-css-transition-replace/static/css/main.747818cd.chunk.css"
+  },
+  {
+    "revision": "0fc4ace7e435245815a4ee954168cb15",
+    "url": "/react-css-transition-replace/index.html"
+  }
+];
