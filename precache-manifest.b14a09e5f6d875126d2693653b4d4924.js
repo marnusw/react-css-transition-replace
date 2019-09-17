e4135664f246ebfcd8489356380be7d07a24ef71
Marnus Weststrate <marnusw@gmail.com>
2019-09-17 08:47:42 +0100

    Using v4.0.0

---
+++ precache-manifest.b14a09e5f6d875126d2693653b4d4924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7578e7a8798ebb2478ab4b2b49bc60bf",
+    "url": "/react-css-transition-replace/index.html"
+  },
+  {
+    "revision": "0cb00990b284d62aa28a",
+    "url": "/react-css-transition-replace/static/css/main.3e9a0ac0.chunk.css"
+  },
+  {
+    "revision": "ae56340736c8eade2858",
+    "url": "/react-css-transition-replace/static/js/2.248f41e6.chunk.js"
+  },
+  {
+    "revision": "0cb00990b284d62aa28a",
+    "url": "/react-css-transition-replace/static/js/main.c8f93c65.chunk.js"
+  },
+  {
+    "revision": "9668b7990f83681763e8",
+    "url": "/react-css-transition-replace/static/js/runtime~main.97b6c9df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-css-transition-replace/precache-manifest.914a9c64cce8b52f218d01e72b69a963.js"
+  "/react-css-transition-replace/precache-manifest.b14a09e5f6d875126d2693653b4d4924.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-css-transition-replace/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-css-transition-replace/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
